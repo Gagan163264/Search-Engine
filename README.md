9958522632369f960948a2a85b06e3568e0fa632
Gagan163264 <gagan.surathkal@gmail.com>
2021-06-06 04:23:53 -0700

    Generalizations and start modular program

---
+++ README.md
@@ -0,0 +1,5 @@
+# Search-Engine
+
+Mini Project for PSWCL-C lab
+
+Create a search engine for academic purposes
--- main.c
+++ main.c
@@ -1,9 +1,28 @@
 #include<stdio.h>
 #include<string.h>
+#include"search.c"
+
 #define INPUT_LIMIT 50
+#define COLUMN_SEPERATOR '|'
+#define ROW_SEPERATOR '\0'
+#define DATABASE "db.txt"
+
+
 
 int main()
 {
+
+  int c;                                      //init
+  int element_str_counter = 0;
+  int line_pos_counter = 0;
+  int line_counter = 1;
+  int match_notfound_flag = 1;
+  char element_str[INPUT_LIMIT];
+  char line_str[512];
+  memset(element_str, 0, sizeof element_str);
+  memset(line_str, 0, sizeof line_str);
+  FILE *file = fopen(DATABASE, "r");
+
 
   char uin_str[INPUT_LIMIT];                 // get user input
   printf("Search For: ");
@@ -11,24 +30,13 @@ int main()
   printf("Running search for: %s", uin_str);
   uin_str[strcspn(uin_str, "\n")] = 0;
 
- int c;                                      //initializations
- int element_str_counter = 0;
- int line_pos_counter = 0;
- int line_counter = 1;
- int match_notfound_flag = 1;
- char element_str[INPUT_LIMIT];
- char line_str[512];
- memset(element_str, 0, sizeof element_str);
- memset(line_str, 0, sizeof line_str);
- FILE *file = fopen("db.txt", "r");
-
  if (file)
  {
    while(fgets(line_str, 512, file))                             //get line from file
     {
-      while((c = line_str[line_pos_counter]) != '\0')           // get character from line
+      while((c = line_str[line_pos_counter]) != ROW_SEPERATOR)           // get character from line
       {
-        if(c == '|' && line_str[line_pos_counter+1] == '|')     //end element at seperator
+        if(c == COLUMN_SEPERATOR && line_str[line_pos_counter+1] == COLUMN_SEPERATOR)     //end element at seperator
         {
           if(!strcmp(element_str, uin_str))
           {

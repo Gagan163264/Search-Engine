fc0aec3124e1873eda1d876c8540b722ba39df61
Gagan163264 <gagan.surathkal@gmail.com>
2021-06-13 01:29:15 -0700

    added input string whitespace trim

--- main.c
+++ main.c
@@ -12,17 +12,33 @@
 int main()
 {
   int total_hit_counter = 0;
+  int start_index = 0;
+  int end_index;
+  int write = 0;
   clock_t timer;
   long database_size = 0;   //init
   char *database;
   database = importdb(&database_size);
 
   char uin_str[INPUT_LIMIT];                 // get user input
+  char raw_uin_str[INPUT_LIMIT];
   printf("Search For: ");
-  fgets(uin_str, INPUT_LIMIT, stdin);
-  printf("Running search for: %s", uin_str);
-  uin_str[strcspn(uin_str, "\n")] = 0;
+  fgets(raw_uin_str, INPUT_LIMIT, stdin);
   timer = clock();
+
+  while(raw_uin_str[start_index] == ' ')    //trim raw input
+    start_index++;
+  end_index = strcspn(raw_uin_str, "\n")-1;
+  while(raw_uin_str[end_index] == ' ')
+    end_index--;
+  while(start_index<=end_index)
+  {
+    uin_str[write++]=raw_uin_str[start_index];
+    start_index++;
+  }
+  uin_str[start_index]=0;
+
+  printf("Running search for: %s\n", uin_str);
   total_hit_counter = simple_search(uin_str, database, database_size);
   timer = clock() - timer;
   double time = ((double)timer)/(CLOCKS_PER_SEC/1000);
